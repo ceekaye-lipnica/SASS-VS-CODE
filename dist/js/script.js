5f568d9a06265589e8e687e9ef9f57af0ae99937
tsutrof <jaysonfortus0815@gmail.com>
2023-05-12 15:23:39 +0800

    services

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,9 @@
 const toggleMenu = document.querySelector(".toggle__menu");
 const headNav = document.querySelector(".header__Nav2");
-// const blurr = document.querySelector(".blur");
+// const blue = document.querySelector(".toggle__menu");
 const header = document.querySelector(".header");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headNav.classList.toggle("open");
-//   blurr.classList.toggle("open");
+  // blue.style="background: red;";
 });
